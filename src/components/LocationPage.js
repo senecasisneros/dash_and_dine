51ada215f8c7030cced43c5d7c438d7cce06d5db
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-21 16:02:43 -0700

    updated LocationPage to add rating, rating stars and rating count

--- src/components/LocationPage.js
+++ src/components/LocationPage.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import WeatherDisplay from './WeatherDisplay';
 import { receiveLocation, changeRes } from '../actions/LocationActions';
 import Maps from './Maps';
-import { Button, Jumbotron } from 'react-bootstrap'
+import { Button, Jumbotron } from 'react-bootstrap';
+import StarRatingComponent from 'react-star-rating-component';
+
 
 class LocationPage extends Component {
   constructor() {
@@ -24,7 +26,7 @@ class LocationPage extends Component {
     if (!this.props.res) {
       return (<h1>Loading...</h1>);
     }
-    const { name, display_phone, url, location, snippet_text, categories, rating } = this.props.res;
+    const { name, display_phone, url, location, snippet_text, categories, rating, review_count } = this.props.res;
     console.log('this.props.res:', this.props.res);
     const { address, city, state_code, postal_code, coordinate } = location;
     const fullAddress = address + ' ' + city + ', ' + state_code + ' ' + postal_code;
@@ -38,14 +40,21 @@ class LocationPage extends Component {
         <Jumbotron className="Jumbotron1 container-fluid">
           <h2 className="companyName">{name}</h2>
           <h6 id="fullAdress">{fullAddress}</h6>
-          <h6 id="fullAdress">{phoneNumber}</h6>
-          <h6 id="fullAdress">Rating: {rating}</h6>
+          <h6 id="phone">{phoneNumber}</h6>
+          <h6 id="cuisineHead">{cuisine}</h6>
+          <p id="reviewCount">{review_count} Reviews</p>
+          <StarRatingComponent
+            name="rating"
+            editing={false}
+            renderStarIcon={() => <span id="starRating">★</span>}
+            starCount={5}
+            value={rating}
+          />
           <div className="center-block">
           <Button id="nextRest" bsStyle="primary" onClick={this._changeRes}>Next Restaurant</Button>
-          <a id="nextRest" className="btn btn-primary" href={url}>Yelp</a>
+          <a id="nextRest" className="btn btn-primary" target="_blank" href={url}>Yelp</a>
           </div>
           <div className="cuisine col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            <h3 className="cuisineHead">{cuisine}</h3>
             <WeatherDisplay main={main} weather_desc={weather_desc} />
           </div>
           <div className="mapDiv col-xs-12 col-sm-12 col-md-6 col-lg-6">
