76dd9aedaf70dfb94da139a3426ea9858bc1ef36
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-05-22 23:18:43 -0700

    update syntax error

--- tools/distServer.js
+++ tools/distServer.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api', require('../routes/api'));
+app.use('/api', require('../server/app'));
 app.get('*', (req, res) => res.sendFile(path.resolve('dist/index.html')));
 
 // --------------------------- Listeners ---------------------------------------
